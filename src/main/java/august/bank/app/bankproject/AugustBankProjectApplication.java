39a6408df7d95a32d1556b4afc48cff757d643a1
Tinsaeberhane <tinsaeberhane.yohannes@miu.edu>
2023-02-13 18:09:02 -0600

    updated

---
+++ src/main/java/august/bank/app/bankproject/AugustBankProjectApplication.java
@@ -0,0 +1,13 @@
+package august.bank.app.bankproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugustBankProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AugustBankProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/august/bank/app/bankproject/AugustBankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package august.bank.app.bankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugustBankProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
